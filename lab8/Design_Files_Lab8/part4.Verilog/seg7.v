3f660685580416f8cdf132ed92411aeea3e3964a
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2022-03-25 02:01:59 -0400

    wrote code for part4

--- lab8/Design_Files_Lab8/part4.Verilog/seg7.v
+++ lab8/Design_Files_Lab8/part4.Verilog/seg7.v
@@ -9,7 +9,11 @@ module seg7 (Data, Addr, Sel, Resetn, Clock, H5, H4, H3, H2, H1, H0);
     assign nData = ~Data;
 
     reg7 reg_R0 (nData, Clock, Resetn, Sel & (Addr == 3'b000), H0);
-    // more code goes here ...
+    reg7 reg_R1 (nData, Clock, Resetn, Sel & (Addr == 3'b001), H1);
+    reg7 reg_R2 (nData, Clock, Resetn, Sel & (Addr == 3'b010), H2);
+    reg7 reg_R3 (nData, Clock, Resetn, Sel & (Addr == 3'b011), H3);
+    reg7 reg_R4 (nData, Clock, Resetn, Sel & (Addr == 3'b100), H4);
+    reg7 reg_R5 (nData, Clock, Resetn, Sel & (Addr == 3'b101), H5);
 endmodule
 
 module reg7 (R, Clock, Resetn, E, Q);
