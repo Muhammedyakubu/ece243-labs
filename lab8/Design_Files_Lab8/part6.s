ba016d3ffb0a7116f9444b521f6a7f667dea31d9
Muhammed Yakubu <72368510+Muhammedyakubu@users.noreply.github.com>
2022-03-26 04:01:33 -0400

    fixed some more stuff in part6

--- lab8/Design_Files_Lab8/part6.s
+++ lab8/Design_Files_Lab8/part6.s
@@ -7,44 +7,44 @@
 .define SPEED_4 0x1F
 
 
-    mv r3, #0x00
-MAIN_LOOP:
-    mvt r4, #LED_ADDRESS
-    // r3 holds the external counter value
-    add r3, #1
-    st  r3, [r4]
+            mv r3, #0x00
 
-    // poll the switches into r1
-    mvt r2, #SW_ADDRESS
-    ld  r1, [r2]
+MAIN_LOOP:  mvt r4, #LED_ADDRESS
+            // r3 holds the external counter value
+            add r3, #1
+            st  r3, [r4]
 
-    sub r1, #1
-    beq SET_SPEED_1
+            // poll the switches into r1
+            mvt r2, #SW_ADDRESS
+            ld  r1, [r2]
 
-    sub r1, #1
-    beq SET_SPEED_2
+            sub r1, #1
+            beq SET_SPEED_1
 
-    sub r1, #1
-    beq SET_SPEED_3
+            sub r1, #1
+            beq SET_SPEED_2
 
-    sub r1, #1
-    beq SET_SPEED_4
+            sub r1, #1
+            beq SET_SPEED_3
 
-    b SET_SPEED_1 // if not any of the above, then set to default speed   
+            sub r1, #1
+            beq SET_SPEED_4
+
+            b SET_SPEED_1 // if not any of the above, then set to default speed   
 
 
 // set delay in r0
 SET_SPEED_1: 
-    mvt r0, #SPEED_1
+    mv r0, #SPEED_1
     b  DELAY
 SET_SPEED_2: 
-    mvt r0, #SPEED_2
+    mv r0, #SPEED_2
     b  DELAY
 SET_SPEED_3:
-    mvt r0, #SPEED_3
+    mv r0, #SPEED_3
     b  DELAY
 SET_SPEED_4:
-    mvt r0, #SPEED_4
+    mv r0, #SPEED_4
     b  DELAY
 
 DELAY:
